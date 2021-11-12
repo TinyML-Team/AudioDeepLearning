f0e209d3f80d75ee463fe88b7289f9e5b02eb61f
Seonghun <dev.seonghun@gmail.com>
2021-11-12 23:42:17 +0900

    add dashboard

---
+++ streamlit.py
@@ -0,0 +1,7 @@
+import streamlit as st
+## Title
+st.title('Streamlit Tutorial')
+st.header('This is header')
+st.subheader('This is subheader')
+st.code('import numpy as np')
+# Display Raw Code — snippet
